ee013c6a1bc7ab8cf94ce9908f65dd2aa90b6440
MaruyamaTomohiro <maruyamatomohiro@maruyamasatoshidainoMacBook-Pro.local>
2022-03-01 12:23:32 +0900

    GitHub_training_Lodash

---
+++ training.js
@@ -0,0 +1,20 @@
+const indexOf = (array, value, fromIndex=0) => {
+    for(let i =fromIndex; i <= array.length; i++){
+        if(array[i] === value){
+            return i;
+        }else if(i === array.length){
+            return -1;
+        }
+    };
+};
+
+
+console.log(indexOf([1, 2, 1, 2], 2));
+// => 1
+ 
+// Search from the `fromIndex`.
+console.log(indexOf([1, 2, 1, 2], 2, 2));
+// => 3
+
+console.log(indexOf([1, 2, 1, 2], 4, 2));
+// => -1
